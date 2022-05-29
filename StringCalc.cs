a9b4b6466c7ebd19edd160c025ad9abb6e97fae6
Sotiris Kotsatos <sotosece@gmail.com>
2022-05-29 19:25:06 +0300

    v2.1

--- StringCalc.cs
+++ StringCalc.cs
@@ -11,20 +11,24 @@ namespace String_Calculator
             var plusMinus = subStrings.FindIndex(x => x.Equals("+") || x.Equals("-"));
             if (multDiv > -1)
             {
+                double prev = Convert.ToDouble(subStrings[multDiv - 1]);
+                double next = Convert.ToDouble(subStrings[multDiv + 1]);
                 if (subStrings[multDiv].Equals("*"))
-                    subStrings[multDiv - 1] = (Convert.ToDouble(subStrings[multDiv - 1]) * Convert.ToDouble(subStrings[multDiv + 1])).ToString();
+                    subStrings[multDiv - 1] = (prev * next).ToString();
                 else
-                    subStrings[multDiv - 1] = (Convert.ToDouble(subStrings[multDiv - 1]) / Convert.ToDouble(subStrings[multDiv + 1])).ToString();
+                    subStrings[multDiv - 1] = (prev / next).ToString();
                 subStrings.RemoveAt(multDiv);
                 subStrings.RemoveAt(multDiv);
                 return Calc(subStrings);
             }
             else if (plusMinus > -1)
             {
+                double prev = Convert.ToDouble(subStrings[plusMinus - 1]);
+                double next = Convert.ToDouble(subStrings[plusMinus + 1]);
                 if (subStrings[plusMinus].Equals("+"))
-                    subStrings[plusMinus - 1] = (Convert.ToDouble(subStrings[plusMinus - 1]) + Convert.ToDouble(subStrings[plusMinus + 1])).ToString();
+                    subStrings[plusMinus - 1] = (prev + next).ToString();
                 else
-                    subStrings[plusMinus - 1] = (Convert.ToDouble(subStrings[plusMinus - 1]) - Convert.ToDouble(subStrings[plusMinus + 1])).ToString();
+                    subStrings[plusMinus - 1] = (prev - next).ToString();
                 subStrings.RemoveAt(plusMinus);
                 subStrings.RemoveAt(plusMinus);
                 return Calc(subStrings);
